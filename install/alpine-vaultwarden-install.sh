5a3dca218e66c3aa0e754a0aee57c9dacd189dc6
CanbiZ <47820557+MickLesk@users.noreply.github.com>
2024-12-10 14:41:59 +0100

    Update alpine-vaultwarden-install.sh

--- install/alpine-vaultwarden-install.sh
+++ install/alpine-vaultwarden-install.sh
@@ -14,23 +14,29 @@ network_check
 update_os
 
 msg_info "Installing Dependencies"
-$STD apk add newt
-$STD apk add curl
-$STD apk add openssl
-$STD apk add openssh
-$STD apk add nano
-$STD apk add mc
-$STD apk add argon2
+$STD apk add newt curl openssl openssh nano mc argon2
 msg_ok "Installed Dependencies"
 
 msg_info "Installing Alpine-Vaultwarden"
 $STD apk add vaultwarden
-sed -i -e 's/# export ADMIN_TOKEN=.*/export ADMIN_TOKEN='\'''\''/' -e '/^# export ROCKET_ADDRESS=0\.0\.0\.0/s/^# //' -e 's|export WEB_VAULT_ENABLED=.*|export WEB_VAULT_ENABLED=true|' /etc/conf.d/vaultwarden
+sed -i -e 's/# export ADMIN_TOKEN=.*/export ADMIN_TOKEN='\'''\''/' \
+       -e '/^# export ROCKET_ADDRESS=0\.0\.0\.0/s/^# //' \
+       -e 's|export WEB_VAULT_ENABLED=.*|export WEB_VAULT_ENABLED=true|' /etc/conf.d/vaultwarden
 msg_ok "Installed Alpine-Vaultwarden"
 
 msg_info "Installing Web-Vault"
 $STD apk add vaultwarden-web-vault
 msg_ok "Installed Web-Vault" 
+
+msg_info "Creating .env file"
+cat <<EOF > /var/lib/vaultwarden/.env
+ROCKET_ADDRESS=0.0.0.0
+DATA_FOLDER=/opt/vaultwarden/data
+DATABASE_MAX_CONNS=10
+WEB_VAULT_FOLDER=/opt/vaultwarden/web-vault
+WEB_VAULT_ENABLED=true
+EOF
+msg_ok "Created .env file"
 
 msg_info "Starting Alpine-Vaultwarden"
 $STD rc-service vaultwarden start
